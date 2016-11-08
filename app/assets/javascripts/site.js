1a16240c71db4181d705b2cc1f4477f148e1e385
Christopher Eaton <eaton.cw@gmail.com>
2016-11-08 13:46:47 -0800

    preview images

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -49,5 +49,16 @@ $(document).on('turbolinks:load', function () {
 		lensSize: 200,
 		responsive: true
 	});
+
+	function readURL(input) {
+		if (input.files && input.files[0]) {
+			var reader = new FileReader();
+
+			reader.onload = function (e) {
+				$('.photo-preview').css('background', 'url(' + e.target.result + ')');
+				reader.readAsDataURL(input.files[0]);
+			}
+		}
+	}
 	
 });
